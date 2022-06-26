968896da9ea3194ea18b0d8fb1616aac713ee3b0
Admin <admin@example.com>
2022-06-26 18:07:22 +0100

    Changes for adding navigation link from env.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'edureka-header',
-  // templateUrl: './header.component.html',
-  template : `<h1>{{applicationName}}</h1>
-    Multiline Example
-  `,
+  templateUrl: './header.component.html',
+  // template : `<h1>{{applicationName}}</h1>
+  //   Multiline Example
+  // `,
   styleUrls: ["../../text.css", './header.component.css']
 })
 export class HeaderComponent implements OnInit {
--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { navigation } from "../../../environments/environment"
 
 @Component({
   selector: 'edureka-navigation',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+
+  public navigation = navigation;
 
   constructor() { }
 

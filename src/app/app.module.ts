088fdadd99e6fae5ff845b96637a40d37de2c4d3
Admin <admin@example.com>
2022-07-16 17:38:33 +0100

    notfound and async pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,7 +31,10 @@ import { environment } from "../environments/environment";
 import { UsersTableComponent } from './components/users-table/users-table.component';
 import { HomepageComponent } from './screens/homepage/homepage.component';
 import { NewsListingPageComponent } from './screens/news-listing-page/news-listing-page.component';
-import { NewsDetailPageComponent } from './screens/news-detail-page/news-detail-page.component'
+import { NewsDetailPageComponent } from './screens/news-detail-page/news-detail-page.component';
+import { PrivacyPolicyPageComponent } from './screens/privacy-policy-page/privacy-policy-page.component';
+import { ContactPageComponent } from './screens/contact-page/contact-page.component';
+import { NotFoundPageComponent } from './screens/not-found-page/not-found-page.component'
 
 // All prod and non prod
 const interceptors = [
@@ -61,7 +64,10 @@ if (!environment.production) {
     UsersTableComponent,
     HomepageComponent,
     NewsListingPageComponent,
-    NewsDetailPageComponent
+    NewsDetailPageComponent,
+    PrivacyPolicyPageComponent,
+    ContactPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
